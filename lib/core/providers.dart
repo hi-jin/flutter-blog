a50bcb340048adc6501adefc1ede3c520d416281
hi-jin <crushed7@o.cnu.ac.kr>
2023-10-02 14:37:59 +0900

    Fix: broken asset path

--- lib/core/providers.dart
+++ lib/core/providers.dart
@@ -5,6 +5,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gitblog/domain/core_settings.dart';
 
 final coreSettingsProvider = FutureProvider<CoreSettings>((ref) async {
-  final coreSettingsString = await rootBundle.loadString("settings/core.json");
+  final coreSettingsString = await rootBundle.loadString("assets/settings/core.json");
   return CoreSettings.fromJson(jsonDecode(coreSettingsString));
 });
