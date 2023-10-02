da2ece7846f660a5b8ef1ead52baa4066d6aada5
hi-jin <crushed7@o.cnu.ac.kr>
2023-10-02 17:00:48 +0900

    Feat: 게시글 작성 기능을 위한 게시글 클래스 #4

--- lib/core/providers.dart
+++ lib/core/providers.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gitblog/domain/core_settings.dart';
+import 'package:gitblog/domain/post_asset.dart';
 
 final coreSettingsProvider = FutureProvider<CoreSettings>((ref) async {
   final coreSettingsString = await rootBundle.loadString("assets/settings/core.json");
@@ -17,4 +18,20 @@ final markdownPostProvider = FutureProvider.family<Markdown, String>((ref, path)
     data: md,
     selectable: true,
   );
+});
+
+// TODO
+final postsProvider = FutureProvider((ref) async {
+  final root = PostDirectory(name: "assets", children: []);
+
+  final AssetManifest manifest = await AssetManifest.loadFromAssetBundle(rootBundle);
+  for (final path in manifest.listAssets()) {
+    print(path);
+
+    if (!path.startsWith("assets/posts/")) continue;
+
+    root.putAsset(path);
+  }
+
+  print(root.toString());
 });
